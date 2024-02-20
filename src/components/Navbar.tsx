87f65e231d914c9ed4fff0080dcbcbc201e02d59
mark <markpackham1@gmail.com>
2024-02-20 11:34:35 +0000

    Update Navbar
    https://youtu.be/KkC_wYM_Co4?si=36c2AQ3EdwGQI-uz&t=1018

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,27 @@
-import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation, MdOutlineLocationOn, MdWbSunny } from "react-icons/md";
 
-type Props = {}
+type Props = {};
 
 export default function Navbar({}: Props) {
-return (
-<nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-    <div className="h-[80px]     w-full    flex   justify-between items-center  max-w-7xl px-3 mx-auto">
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px]     w-full    flex   justify-between items-center  max-w-7xl px-3 mx-auto">
         <p className="flex items-center justify-center gap-2">
-            <h2 className="text-gray-500 text-3xl">Weather</h2>
-            <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
+          <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
-    </div>
-</nav>
-)
+        <section className="flex gap-2 items-center">
+          <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdOutlineLocationOn className="text-3xl"/>
+          {/* "text-slate-900/80" the last part means means giving an opacity of 80 */}
+          <p className="text-slate-900/80 text-sm"> UK </p>
+          
+          {/* Search Bar */}
+          <div>
+
+          </div>
+        </section>
+      </div>
+    </nav>
+  );
 }
