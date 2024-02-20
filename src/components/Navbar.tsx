ce9063c9c19a6c012ece14464c0016de5e905432
mark <markpackham1@gmail.com>
2024-02-20 11:19:18 +0000

    Add sun icon to weather header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { MdWbSunny } from "react-icons/md";
+
 type Props = {}
 
 export default function Navbar({}: Props) {
@@ -6,6 +8,7 @@ return (
     <div className="h-[80px]     w-full    flex   justify-between items-center  max-w-7xl px-3 mx-auto">
         <p className="flex items-center justify-center gap-2">
             <h2 className="text-gray-500 text-3xl">Weather</h2>
+            <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
     </div>
 </nav>
