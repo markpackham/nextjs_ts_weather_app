1fb617804d93313947f4d4e40169c7153e47aee5
mark <markpackham1@gmail.com>
2024-02-20 19:23:32 +0000

    Setup fetch query for API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
+import { useQuery } from "react-query";
 
 interface WeatherDetail {
   dt: number;
@@ -56,6 +59,14 @@ interface WeatherData {
 }
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery("repoData", async () =>
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=london&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+    ).then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
