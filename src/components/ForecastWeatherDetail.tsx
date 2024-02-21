56e28fb927e2b8e5ca26000ab64d26521fb298b4
mark <markpackham1@gmail.com>
2024-02-21 17:29:01 +0000

    Create ForecastWeatherDetailsProps interface extending WeatherDetailProps
    https://youtu.be/KkC_wYM_Co4?feature=shared&t=5607

--- src/components/ForecastWeatherDetail.tsx
+++ src/components/ForecastWeatherDetail.tsx
@@ -1,7 +1,26 @@
 import Container from "./Container";
+import { WeatherDetailProps } from "./WeatherDetails";
+import WeatherIcon from "./WeatherIcon";
 
-type Props = {};
+export interface ForecastWeatherDetailProps extends WeatherDetailProps {
+  weatehrIcon: string;
+  date: string;
+  day: string;
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  description: string;
+}
 
-export default function ForecastWeatherDetail({}: Props) {
-  return <Container></Container>;
+export default function ForecastWeatherDetail({}: ForecastWeatherDetailProps) {
+  return (
+    <Container className="gap-4">
+      <section className="flex gap-4 items-center px-4">
+        <div>
+          <WeatherIcon />
+        </div>
+      </section>
+    </Container>
+  );
 }
