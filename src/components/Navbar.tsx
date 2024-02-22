90a90914547e9f8b90b12b23328b469d4cd64a98
mark <markpackham1@gmail.com>
2024-02-22 12:17:52 +0000

    Provide parameter for handleInputChange
    https://youtu.be/KkC_wYM_Co4?feature=shared&t=6953

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar({}: Props) {
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
 
-  // City Search
+  // City Search after suggestion selected
   function handleSubmitSearch() {}
 
   // React when city typed in
@@ -49,6 +49,7 @@ export default function Navbar({}: Props) {
     }
   }
 
+  // Handle suggestion user clicked on
   function handleSuggestionClick(value: string) {
     setCity(value);
     setShowSuggestions(false);
@@ -72,10 +73,11 @@ export default function Navbar({}: Props) {
             <SearchBox
               value={city}
               onSubmit={handleSubmitSearch}
-              onChange={(e) => handleInputChange()}
+              onChange={(e) => handleInputChange(e.target.value)}
             />
 
             <SuggestionBox
+              // Use spreach operator to avoid having to write all props one by one, error={error}
               {...{
                 showSuggestions,
                 suggestions,
@@ -90,6 +92,7 @@ export default function Navbar({}: Props) {
   );
 }
 
+// List out city suggestions for searching
 function SuggestionBox({
   showSuggestions,
   suggestions,
@@ -103,11 +106,14 @@ function SuggestionBox({
 }) {
   return (
     <>
+      {/* Either show if we do have suggestions or if an error has popped up */}
       {((showSuggestions && suggestions.length > 1) || error) && (
         <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
+          {/* Show error */}
           {error && suggestions.length < 1 && (
             <li className="text-red-500 p-1 ">{error}</li>
           )}
+          {/* Output the suggestions */}
           {suggestions.map((item, index) => (
             <li
               key={index}
