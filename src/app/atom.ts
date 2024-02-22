ede44a07d1f6b5c0805c95acffc148e13e7bbcc3
mark <markpackham1@gmail.com>
2024-02-22 15:03:03 +0000

    Add global scope loadingCityAtom boolean

--- src/app/atom.ts
+++ src/app/atom.ts
@@ -2,3 +2,5 @@ import { atom } from "jotai";
 
 // Handles global state similar to store with Redux
 export const placeAtom = atom("Republic of India");
+
+export const loadingCityAtom = atom(false);
