8a747bc325c44b7f14a4946dd0618915b51e29c7
mark <markpackham1@gmail.com>
2024-02-22 15:49:34 +0000

    Relocate WeatherSkeleton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import { useAtom } from "jotai";
 import { useQuery } from "react-query";
 import { loadingCityAtom, placeAtom } from "./atom";
 import { useEffect } from "react";
+import WeatherSkeleton from "@/components/WeatherSkeleton";
 
 interface WeatherDetail {
   dt: number;
@@ -295,46 +296,5 @@ export default function Home() {
         ;
       </main>
     </div>
-  );
-}
-
-// Skeleton loading component whilst waiting for the data to show
-function WeatherSkeleton() {
-  return (
-    <section className="space-y-8 ">
-      {/* Today's data skeleton */}
-      <div className="space-y-2 animate-pulse">
-        {/* Date skeleton */}
-        <div className="flex gap-1 text-2xl items-end ">
-          <div className="h-6 w-24 bg-gray-300 rounded"></div>
-          <div className="h-6 w-24 bg-gray-300 rounded"></div>
-        </div>
-
-        {/* Time wise temperature skeleton */}
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {[1, 2, 3, 4].map((index) => (
-            <div key={index} className="flex flex-col items-center space-y-2">
-              <div className="h-6 w-16 bg-gray-300 rounded"></div>
-              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
-              <div className="h-6 w-16 bg-gray-300 rounded"></div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* 7 days forecast skeleton */}
-      <div className="flex flex-col gap-4 animate-pulse">
-        <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
-
-        {[1, 2, 3, 4, 5, 6, 7].map((index) => (
-          <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
-            <div className="h-8 w-28 bg-gray-300 rounded"></div>
-            <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
-            <div className="h-8 w-28 bg-gray-300 rounded"></div>
-            <div className="h-8 w-28 bg-gray-300 rounded"></div>
-          </div>
-        ))}
-      </div>
-    </section>
   );
 }
